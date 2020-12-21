0dfc503792b6cededb644d2605cd1787e5b9b526
Iván Landaverde <2502882019@mail.utec.edu.sv>
2020-12-21 14:02:16 -0600

    REACT

---
+++ src/components/Name.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Name(){
+    return(
+        <>
+            <h2>Ivan Mendoza</h2>
+            <span>Web development</span>
+            <div>
+                <p>lorem50</p>
+                <ul>
+                    <li>BICHITA</li>
+                    <li>BBCITA</li>
+                    <li>BRRRRRRR</li>
+                </ul>
+            </div>
+        </>
+    )
+}
